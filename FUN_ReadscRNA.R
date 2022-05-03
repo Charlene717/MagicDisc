c0dc21f215bbad2af34f0752bc7a43ac97f26b1c
CharleneMSI <q2330416@gmail.com>
2022-05-03 10:48:12 +0800

    Add choice of H5AD import file.

--- FUN_ReadscRNA.R
+++ FUN_ReadscRNA.R
@@ -7,53 +7,74 @@ ReadscRNA = function( InputFolder, list_files.df, Mode="10x" ,ProjectName="") # 
       for(i in 1:nrow(list_files.df)){
         Folder <- list_files.df$Folder[i]
         Data.dgCMatrix <- Read10X(data.dir = paste0(InputFolder,"/", Folder, "/monocle/outs/filtered_gene_bc_matrices/mm10"))
-        Data.SeuObj <- CreateSeuratObject(counts = Data.dgCMatrix, 
-                                          project = ProjectName, 
+        Data.SeuObj <- CreateSeuratObject(counts = Data.dgCMatrix,
+                                          project = ProjectName,
                                           min.cells = 3, min.features = 200)
-        
+
         for (j in 1:(ncol(list_files.df)-1)) {
-          Data.SeuObj@meta.data[[colnames(list_files.df)[j+1]]] <- rep(list_files.df[i,j+1], 
-                                                                       times=length(Data.SeuObj@meta.data[["orig.ident"]]))
+          Data.SeuObj@meta.data[[colnames(list_files.df)[j+1]]] <- rep(list_files.df[i,j+1],
+                                                                       times=nrow(Data.SeuObj@meta.data))
         }
-        
+
         scRNA_SeuObj.list[[i]] <- Data.SeuObj
         names(scRNA_SeuObj.list)[[i]] <- list_files.df$Folder[i]
       }
       rm(i,j,Folder,Data.dgCMatrix,Data.SeuObj)
-      
+
+    }else if(Mode=="H5AD") {
+      scRNA_SeuObj.list <- list()
+      for(i in 1:nrow(list_files.df)){
+        # Install SeuratDisk # https://github.com/mojaveazure/seurat-disk
+        library(SeuratDisk)
+        Folder <- list_files.df$Folder[i]
+        ## Convert h5ad to h5seurat
+        Convert(paste0(InputFolder,"/", Folder, "/scRNA.h5ad"), paste0(InputFolder,"/", Folder, "/scRNA.h5seurat"),
+                assay = "RNA",) # This creates a copy of this .h5ad object reformatted into .h5seurat inside the example_dir directory
+
+        Data.SeuObj <- LoadH5Seurat(paste0(InputFolder,"/", Folder, "/scRNA.h5seurat")) # This .d5seurat object can then be read in manually
+
+        for (j in 1:(ncol(list_files.df)-1)) {
+          Data.SeuObj@meta.data[[colnames(list_files.df)[j+1]]] <- rep(list_files.df[i,j+1],
+                                                                       times=nrow(Data.SeuObj@meta.data))
+        }
+
+        scRNA_SeuObj.list[[i]] <- Data.SeuObj
+        names(scRNA_SeuObj.list)[[i]] <- list_files.df$Folder[i]
+      }
+
+
     }else{
-      
+
       ## Expression matrix
       ## GSE103322 HNSC
       scRNA_SeuObj.list <- list()
       for(i in 1:nrow(list_files.df)){
         Folder <- list_files.df$Folder[i]
-        GeneExp.df <- read.table(paste0(InputFolder,"/", Folder, "/Exp.tsv"),
+        GeneExp.df <- read.table(paste0(InputFolder,"/", Folder, "/ExpMat.tsv"),
                                      header=T, row.names = 1, sep="\t")
         Data.SeuObj <- CreateSeuratObject(counts = GeneExp.df,
                                           project = ProjectName,
                                           min.cells = 3, min.features = 200)
-        
+
         for (j in 1:(ncol(list_files.df)-1)) {
-          Data.SeuObj@meta.data[[colnames(list_files.df)[j+1]]] <- rep(list_files.df[i,j+1], 
-                                                                       times=length(Data.SeuObj@meta.data[["orig.ident"]]))
+          Data.SeuObj@meta.data[[colnames(list_files.df)[j+1]]] <- rep(list_files.df[i,j+1],
+                                                                       times=nrow(Data.SeuObj@meta.data))
         }
-        
+
         scRNA_SeuObj.list[[i]] <- Data.SeuObj
         names(scRNA_SeuObj.list)[[i]] <- list_files.df$Folder[i]
-        
-        scAnno.df <- read.table(paste0(InputFolder,"/", Folder, "/Anno.tsv"),
+
+        scAnno.df <- read.table(paste0(InputFolder,"/", Folder, "/CellMeta.tsv"),
                                      header=T, row.names = 1, sep="\t")
         # scRNA.SeuObj@meta.data[["sample"]] <- GeneExp.df[5,] %>% as.character()
-        
+
           for (k in 1:nrow(scAnno.df)) {
             scRNA_SeuObj.list[[i]]@meta.data[[row.names(scAnno.df)[k]]] <- c(scAnno.df[k,] %>% as.character())
           }
-        
+
       }
       rm(i,j,Folder,GeneExp.df,Data.SeuObj)
- 
+
     }
     return(scRNA_SeuObj.list)
   }
-    
