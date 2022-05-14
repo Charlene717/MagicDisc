a5a2bd3368c2514e55b0ed44a13dce6702b6889f
CharleneMSI <q2330416@gmail.com>
2022-05-14 22:30:29 +0800

    Modify the heatmap

--- #_MagicDisc_Main.R
+++ #_MagicDisc_Main.R
@@ -185,84 +185,16 @@
   PBMC.markers <- FindAllMarkers(scRNA.SeuObj, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 
   scRNA.SeuObj <- Beautify_Heatmap_Seurat(scRNA.SeuObj, PBMC.markers, topN = 7, Path = PathCluster,
+                                          Type = "seurat_clusters",
                                           projectName = ProjectName)
 
 
   # --------------- Check specific tissue marker --------------- #
+  #(Pending)
+  UMAP_CellTypeMarker()
 
-  pdf(
-    file = paste0(PathCellType,"_CT/",ProjectName,"_nlDR_CTMarker.pdf"),
-    width = 10,  height = 8
-  )
-
-    # PMID: 31771616 #!!!!!!
-    FeaturePlot(scRNA.SeuObj, features = c("Cd3d", "Cd4", "Cd8a", "Csf1r", "Foxp3", "S100a9"), min.cutoff = "q9",
-                ncol = 3, coord.fixed = 1)
-    # T Cell: Cd3d;  CD4+ T Cell: Cd4; CD8+ T Cell: Cd8a; Macrophages: Csf1r; regulatory T cells(Treg): Foxp3; Neutrophils: S100a9
-
-    # PMID: 34296197 #!!!!!
-    FeaturePlot(scRNA.SeuObj, features = c("Cd3d", "Cd3e", "Lyz1", "Lyz2","Clu","Cd79a","Ms4a1","Nkg7","Gzmb"), min.cutoff = "q9",
-                ncol = 3, coord.fixed = 1)
-    # T Cell: Cd3d,Cd3e;  Macrophages: Lyz; Mast Cell: Clu; B Cell: Cd79a,Ms4a1; NK Cell: Nkg7,Gzmb
-
-    # http://biocc.hrbmu.edu.cn/CellMarker/
-    # Mast cell
-    FeaturePlot(scRNA.SeuObj, features = c("Cd117", "Cd25","Cd203c","Slc18a2","Kit","Fcer1a","Cd9"), min.cutoff = "q9", coord.fixed = 1)
-    # PMID: 30356731
-    FeaturePlot(scRNA.SeuObj, features = c("Cd9"), min.cutoff = "q9", coord.fixed = 1)
-    # PMID: 34296197 #!!!!!
-    FeaturePlot(scRNA.SeuObj, features = c("Cpa3"), min.cutoff = "q9", coord.fixed = 1)
-    # https://www.panglaodb.se/markers.html?cell_type=%27Mast%20cells%27
-
-    ## Mac
-    # Macrophage-Markers
-    # https://www.biocompare.com/Editorial-Articles/566347-A-Guide-to-Macrophage-Markers/
-    ## M0
-    FeaturePlot(scRNA.SeuObj, features = c("Cd68", "Adgre1","Cd14","Csf1r","Ly6c1",
-                                            "Cx3cr1","Fcgr1a","Itgam","Mertk"), min.cutoff = "q9", coord.fixed = 1)
-
-    ## M1
-    # M1 http://biocc.hrbmu.edu.cn/CellMarker/
-    FeaturePlot(scRNA.SeuObj, features = c("Cd16","Cd32","Cd64","Cd68","Cd80","Cd86"), min.cutoff = "q9", coord.fixed = 1)
-    # M1 https://www.biocompare.com/Editorial-Articles/566347-A-Guide-to-Macrophage-Markers/
-    FeaturePlot(scRNA.SeuObj, features = c("Marco","Nos2","Tlr2","Cd80","Cd86","Csf2",
-                                            "Tnf","Il1b","Il6","Tlr4","Cxcl2","Ifng","Il1r1"), min.cutoff = "q9", coord.fixed = 1)
-    FeaturePlot(scRNA.SeuObj, features = c("Il1a","Il1b","Il6","Nos2","Tlr2","Tlr4","Cd80","Cd86"), min.cutoff = "q9", coord.fixed = 1)
-
-
-    ## M2
-    # M2 http://biocc.hrbmu.edu.cn/CellMarker/
-    FeaturePlot(scRNA.SeuObj, features = c("Chil3","Csf1r","Mrc1","Pparg","Arg1","Cd163","Clec10a","Clec7a",
-                                            "Cd206","Cd209","Ccl18","Fizz1"), min.cutoff = "q9", coord.fixed = 1)
-    # https://www.biocompare.com/Editorial-Articles/566347-A-Guide-to-Macrophage-Markers/
-    FeaturePlot(scRNA.SeuObj, features = c("Cd115", "Cd206", "Pparg", "Arg1", "Cd163", "Cd301",
-                                            "Dectin-1", "Pdcd1lg2", "Fizz1"), min.cutoff = "q9", coord.fixed = 1)
-
-    FeaturePlot(scRNA.SeuObj, features = c("Chil3"), min.cutoff = "q9", coord.fixed = 1)
-
-
-
-    ## Tumor associated macrophage(TAM)
-    FeaturePlot(scRNA.SeuObj, features = c("Ccr2","Csf1r","Marco","Pdl2","Cd40","Ccl2","Csf1","Cd16"),
-                min.cutoff = "q9", coord.fixed = 1)
-
-    # Erythrocytes
-    FeaturePlot(scRNA.SeuObj, features = c("Hbb-bs"), min.cutoff = "q9", coord.fixed = 1)
-    # Platelet
-    FeaturePlot(scRNA.SeuObj, features = c("Ppbp"), min.cutoff = "q9", coord.fixed = 1)
-
-    ## Summary
-    markers.to.plot <- c("Cd3d","Cd3e", "Cd4","Cd8a", "Csf1r", "Lyz2","Chil3","Il1b", "S100a9","Nkg7",
-                         "Gzmb", "Cd79a", "Ms4a1","Clu","Hbb-bs","Ppbp")
-
-    FeaturePlot(scRNA.SeuObj, features = markers.to.plot, min.cutoff = "q9", coord.fixed = 1)
-    # T Cell: Cd3d,Cd3e;  CD4+ T Cell: Cd4; CD8+ T Cell: Cd8a; Macrophages: Csf1r,Lyz,Chil3;  Neutrophils: S100a9;
-    # NK Cell: Nkg7,Gzmb; B Cell: Cd79a,Ms4a1; Mast Cell: Clu; Erythrocytes: Hbb-bs; Platelet: Ppbp
-
-
-  dev.off()
-
-save.image(paste0(Save.Path,"/05_Identify_conserved_cell_type_markers.RData"))
+  #### Save RData ####
+  save.image(paste0(Save.Path,"/05_Identify_conserved_cell_type_markers.RData"))
 
 ##### 06 Cell type annotation  #####
   # scRNA.SeuObj.copy <- scRNA.SeuObj
@@ -282,48 +214,11 @@ save.image(paste0(Save.Path,"/05_Identify_conserved_cell_type_markers.RData"))
 
   ## Heatmap
   Heatmap_Color.lt <- list(low="#5283ff",mid ="white", high ="#ff5c5c")
-
-  pdf(
-    file = paste0(PathCellType,"/",ProjectName,"_Heatmap_CellType_top",top_NSet,".pdf"),
-    width = 10,  height = 8
-  )
-    DoHeatmap(scRNA.SeuObj, features = top_N$gene,size = 3,angle = 60) +
-      scale_fill_gradient2(low = Heatmap_Color.lt[["low"]],
-                           mid = Heatmap_Color.lt[["mid"]],
-                           high = Heatmap_Color.lt[["high"]]) +
-      theme(axis.text.y = element_text(size  = 5)) +
-      theme(legend.position = "bottom")+
-      theme(aspect.ratio=1)
-  dev.off()
-
-  # ## Ch
-  # # https://github.com/satijalab/seurat/issues/2960
-  # set.seed(1) # Fix the seed
-  # scRNA.SeuObj <- ScaleData(scRNA.SeuObj, verbose = FALSE)
-  DoHeatmap(scRNA.SeuObj, features = top_N$gene,size = 4,angle = 90) + NoLegend()
-  DoHeatmap(scRNA.SeuObj, features = top_N$gene,group.by = "celltype",size = 4,angle = 90) + NoLegend()
-
-  # Color
-  DoHeatmap(scRNA.SeuObj, features = top_N$gene,group.by = "celltype",size = 3,angle = 60) +
-    scale_fill_gradient2(low = Heatmap_Color.lt[["low"]],
-                         mid = Heatmap_Color.lt[["mid"]],
-                         high = Heatmap_Color.lt[["high"]])
+  scRNA.SeuObj <- Beautify_Heatmap_Seurat(scRNA.SeuObj, PBMC.markers, topN = 7, Path = PathCluster,
+                                          Type = "celltype", HMColor.lt = Heatmap_Color.lt,
+                                          projectName = ProjectName)
 
 
-  DoHeatmap(scRNA.SeuObj, features = top_N$gene,group.by = "seurat_clusters",size = 3,angle = 90) + NoLegend()
-
-  pdf(
-    file = paste0(PathCellType,"/",ProjectName,"_Heatmap_CellType_top",top_NSet,".pdf"),
-    width = 10,  height = 8
-  )
-    DoHeatmap(scRNA.SeuObj, features = top_N$gene,group.by = "celltype",size = 2,angle = 45) +
-      scale_fill_gradient2(low = Heatmap_Color.lt[["low"]],
-                           mid = Heatmap_Color.lt[["mid"]],
-                           high = Heatmap_Color.lt[["high"]])  +
-      theme(axis.text.y = element_text(size = 5)) +
-      theme(legend.position = "bottom" )
-
-  dev.off()
 
   ## UMAP tSNE
   DimPlot(scRNA.SeuObj, label = TRUE) %>% BeautifyggPlot(.,LegPos = c(1, 0.5))
--- FUN_Beautify_Heatmap_Seurat.R
+++ FUN_Beautify_Heatmap_Seurat.R
@@ -1,6 +1,10 @@
 Beautify_Heatmap_Seurat <- function(scRNA.SeuObj, PBMC.markers, topN = 7,
-                                    Path = PathCluster, 
-                                    projectName = ProjectName
+                                    Path = PathCluster, projectName = ProjectName,
+                                    Type = "seurat_clusters",
+                                    HMColor.lt = list(low="#5283ff",mid ="white", high ="#ff5c5c"),
+                                    Xangle = 60, XtextSize = 2, YtextSize = 5,
+                                    LegPos = "bottom"
+                                    
                                     ) {
   
   
@@ -17,23 +21,30 @@ Beautify_Heatmap_Seurat <- function(scRNA.SeuObj, PBMC.markers, topN = 7,
   
   ## Export pdf
   pdf(
-    file = paste0(Path, "/",projectName,"_Heatmap_Cluster_top",top_NSet,".pdf"),
+    file = paste0(Path, "/",projectName,"_Heatmap_",Type,"_top",top_NSet,".pdf"),
     width = 10,  height = 8
   )
-  HM <- DoHeatmap(scRNA.SeuObj, features = top_N$gene,size = 2,angle = 60) +
-        scale_fill_gradient2(low="#5283ff",mid ="white", high ="#ff5c5c") +
-        theme(axis.text.y = element_text(size  = 5)) +
-        theme(legend.position = "bottom" )
-  
+  HM <- DoHeatmap(scRNA.SeuObj, features = top_N$gene, group.by = Type,
+                  size = XtextSize, angle = Xangle) +
+        scale_fill_gradient2(low = HMColor.lt$low,
+                             mid = HMColor.lt$mid, 
+                             high =  HMColor.lt$high) +
+        theme(axis.text.y = element_text(size  = YtextSize)) +
+        theme(legend.position = LegPos )
   print(HM)
+  
+  HM2 <- HM + NoLegend()
+  print(HM2)
+  
   dev.off()
   
+  print(HM)
   ## Export txt
   write.table(data.frame(Num = row.names(top_N),top_N), 
-              file=paste0(Path, "/",projectName,"_ClusterMarker_top",top_NSet,"Gene.txt"),sep="\t", 
+              file=paste0(Path, "/",projectName,"_",Type,"Marker_top",top_NSet,"Gene.txt"),sep="\t", 
               row.names=T, quote = FALSE)
   write.table(data.frame(Gene = row.names(PBMC.markers), PBMC.markers), 
-              file=paste0(Path, "/",projectName,"_ClusterMarker_AllGene.txt"),sep="\t", 
+              file=paste0(Path, "/",projectName,"_",Type,"Marker_AllGene.txt"),sep="\t", 
               row.names=F,quote = FALSE)
   
   return(scRNA.SeuObj)
