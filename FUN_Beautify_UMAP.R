76b63a4a54deeaf0013357c4920884efd36634ab
CharleneMSI <q2330416@gmail.com>
2022-05-14 18:54:28 +0800

    Debug UMAP plot

--- FUN_Beautify_UMAP.R
+++ FUN_Beautify_UMAP.R
@@ -1,22 +1,23 @@
-BeautifyUMAP = function( P1,FileName = "NO1",RVer = Version,
-                           LegPos = c(1.01, 0.5),LegBox = "vertical",LegDir="vertical",
-                           LegTextSize = 22,
-                           TH= 0.5,TV= 0.5, TitleSize = 30,
-                           XtextSize=25,  YtextSize=25,  XaThick=0.9,  YaThick=0.9, xangle =0,
-                           AxisTitleSize=1.5, AspRat=1,SubTitSize = 15 
-                           ){
-  
+BeautifyUMAP = function( P1,RVer = Version,
+                         FileName = "NO1",
+                         LegPos = c(1.01, 0.5),LegBox = "vertical",LegDir="vertical",
+                         LegTextSize = 22,
+                         TH= 0.5,TV= 0.5, TitleSize = 30,
+                         XtextSize=25,  YtextSize=25,  XaThick=0.9,  YaThick=0.9, xangle =0,
+                         AxisTitleSize=1.5, AspRat=1,SubTitSize = 15
+                         ){
+
   library(ggplot2)
   library(graphics)
-  
-  P2 <-  P1 +            
+
+  P2 <-  P1 +
     theme(axis.text.x = element_text(size = XtextSize,angle = xangle, hjust = 1, vjust = .5), #face="bold", # Change the size along the x axis
           axis.text.y = element_text(size = YtextSize), # Change the size along the y axis
-          
-          axis.line = element_line(colour = "darkblue", size = 2, linetype = "solid"), 
+
+          axis.line = element_line(colour = "darkblue", size = 2, linetype = "solid"),
           axis.title = element_text(size = rel(AxisTitleSize),face="bold"),
-          plot.title = element_text(color="black", 
-                                    size=TitleSize, 
+          plot.title = element_text(color="black",
+                                    size=TitleSize,
                                     face="bold.italic",
                                     hjust = TH,vjust =TV), # margin = margin(t = 0.5, b = -7),
           #     plot.background = element_rect(fill = 'chartreuse'),
@@ -30,11 +31,11 @@ BeautifyUMAP = function( P1,FileName = "NO1",RVer = Version,
           axis.line.y = element_line(colour = "black", size = YaThick))+
     theme(legend.position = LegPos , legend.box = LegBox ,legend.direction=LegDir)+ # vertical, horizontal
     #theme(legend.position = "top" , legend.direction = LegDir) # vertical, horizontal
-    theme(strip.text = element_text(size=SubTitSize))+ 
+    theme(strip.text = element_text(size=SubTitSize))+
     theme(panel.background = element_rect(colour = "black",size = 1.5))
 
   ##
-  P3 <- P1 + theme(panel.grid.major =element_blank(), 
+  P3 <- P1 + theme(panel.grid.major =element_blank(),
                     panel.grid.minor = element_blank(),
                     panel.background = element_blank(),#去除背景
                     panel.border = element_blank(), #去除邊框
@@ -51,29 +52,29 @@ BeautifyUMAP = function( P1,FileName = "NO1",RVer = Version,
                     axis.text.x = element_blank(),
                     axis.ticks.y = element_blank(),
                     axis.text.y = element_blank())
-    
-  
+
+
   ## Export PDF
   pdf(
-    file = paste0(getwd(),FileName,".pdf"),
+    file = paste0(FileName,".pdf"),
     width = 12,  height = 8
   )
   print(P2)
   print(P3)
   dev.off()
   # graphics.off()
-  
+
   ## Export tiff
   tiff(
-    file = paste0(getwd(),FileName,"01.tiff"), 
+    file = paste0(FileName,"01.tiff"),
     width = 28, height = 17, units = "cm", res = 200)
     print(P2)
    graphics.off()
   tiff(
-    file = paste0(getwd(),FileName,"02.tiff"), 
+    file = paste0(FileName,"02.tiff"),
     width = 17, height = 17, units = "cm", res = 200)
    print(P3)
    graphics.off()
-  
+
   return(P2)
 }
