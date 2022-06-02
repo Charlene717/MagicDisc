15d2e05767656c860ff14bedd12454d4b66cf7ab
CharleneMSI <q2330416@gmail.com>
2022-06-02 22:49:14 +0800

    Add path and filename setting.

--- TidyGTF.R
+++ TidyGTF.R
@@ -1,8 +1,10 @@
 
 ## Ref: https://www.biostars.org/p/272889/
 ## Ref: https://bioconductor.org/packages/release/bioc/html/rtracklayer.html
+GenecodePath <- "./Input_files/Genecode"
+GenecodeFilename <- "gencode.vM29.annotation"
 
-gtf <- rtracklayer::import('./Input_files/Genecode/gencode.vM29.annotation.gtf')
+gtf <- rtracklayer::import(paste0(GenecodePath,"/",GenecodeFilename,".gtf"))
 gtf_df = as.data.frame(gtf) %>%
             dplyr::filter(type=="gene") %>%
             relocate(gene_name,.before = seqnames) %>%
@@ -11,7 +13,7 @@ gtf_df = as.data.frame(gtf) %>%
 gtf_df <- gtf_df[,-5]
 
 
-write.table(gtf_df, "./Input_files/Genecode/gencode.vM29.annotation.txt",
+write.table(gtf_df, paste0(GenecodePath,"/",GenecodeFilename,".txt"),
             sep = "\t", row.names = F,quote = FALSE, col.names = F)
 
 # TTT <- read.delim(paste0(getwd(),"/Input_files/Genecode/gencode.vM29.annotation.txt"),header  = F)
