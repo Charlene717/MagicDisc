b1d0d10b28ec0e97492bb78b08e27bb587a064db
CharleneMSI <q2330416@gmail.com>
2022-06-25 18:11:51 +0800

    Clean up the code.

--- 20220623_TestCombine.R
+++ 20220623_TestCombine.R
@@ -1,33 +1,33 @@
 ##### Presetting ######
-rm(list = ls()) # Clean variable
-memory.limit(300000)
+  rm(list = ls()) # Clean variable
+  memory.limit(300000)
 
 
-#
-# i=160
-# j=0.3
-# k=25
-# seuratObject@reductions[["umap"]]@cell.embeddings <- seuratObject@meta.data[[paste0("UMAP_PCA",i,"_NNe",k,"_MD03",j)]]
-#
-#
-# FeaturePlot(seuratObject, features = c("TOP2A"))
-# FeaturePlot(seuratObject, features = c("ALDH1A1","ALDH1A2","ALDH1A3","NR5A2","KRT19","SOX9"))
-#
-#
-#
-#
-# FeaturePlot(seuratObject, features = c("HNF1B","HNF6","FOXA2","HNF4A","HEX","GATA4","GATA6"))
-# FeaturePlot(seuratObject, features = c("MNX1","PTF1A","PDX1","HNF1B","HNF6","FOXA2","HNF4",
-#                                        "HEX","GATA4","GATA6"))
-#
-# FeaturePlot(seuratObject, features = c("SOX9","NKX6-1","NKX2-2","MNX1","PTF1A","PDX1",
-#                                        "HNF1B","HNF6","FOXA2","HNF4","HEX","GATA4","GATA6"))
-#
-# FeaturePlot(seuratObject, features = c("HES1","PROX1","SOX9","NKX6-1","NKX2-2","PTF1A","PDX1",
-#                                        "HNF1B","HNF6","FOXA2","HNF4A","GATA4","GATA6"))
-#
-# FeaturePlot(seuratObject, features = c("PTF1A","CPA1","MYC","NR5A2","HNF1B","MNX1","SOX9"))
-# FeaturePlot(seuratObject, features = c("HNF1B","HNF6","FOXA2","NR5A2","GLIS3","PROX1"))
+  #
+  # i=160
+  # j=0.3
+  # k=25
+  # seuratObject@reductions[["umap"]]@cell.embeddings <- seuratObject@meta.data[[paste0("UMAP_PCA",i,"_NNe",k,"_MD03",j)]]
+  #
+  #
+  # FeaturePlot(seuratObject, features = c("TOP2A"))
+  # FeaturePlot(seuratObject, features = c("ALDH1A1","ALDH1A2","ALDH1A3","NR5A2","KRT19","SOX9"))
+  #
+  #
+  #
+  #
+  # FeaturePlot(seuratObject, features = c("HNF1B","HNF6","FOXA2","HNF4A","HEX","GATA4","GATA6"))
+  # FeaturePlot(seuratObject, features = c("MNX1","PTF1A","PDX1","HNF1B","HNF6","FOXA2","HNF4",
+  #                                        "HEX","GATA4","GATA6"))
+  #
+  # FeaturePlot(seuratObject, features = c("SOX9","NKX6-1","NKX2-2","MNX1","PTF1A","PDX1",
+  #                                        "HNF1B","HNF6","FOXA2","HNF4","HEX","GATA4","GATA6"))
+  #
+  # FeaturePlot(seuratObject, features = c("HES1","PROX1","SOX9","NKX6-1","NKX2-2","PTF1A","PDX1",
+  #                                        "HNF1B","HNF6","FOXA2","HNF4A","GATA4","GATA6"))
+  #
+  # FeaturePlot(seuratObject, features = c("PTF1A","CPA1","MYC","NR5A2","HNF1B","MNX1","SOX9"))
+  # FeaturePlot(seuratObject, features = c("HNF1B","HNF6","FOXA2","NR5A2","GLIS3","PROX1"))
 
 
 
@@ -37,39 +37,39 @@ memory.limit(300000)
 
 
 ##### Data prepocessing #####
-load("D:/Dropbox/##_GitHub/##_Charlene/TrajectoryAnalysis/SeuratObject_CDS_PRJCA001063_V2.RData")
-seuratObject_1 <- scRNA.SeuObj
-seuratObject_1@meta.data[["DataSetID"]] <- rep("PRJCA001063")
-rm(list=setdiff(ls(), "seuratObject_1"))
+  load("D:/Dropbox/##_GitHub/##_Charlene/TrajectoryAnalysis/SeuratObject_CDS_PRJCA001063_V2.RData")
+  seuratObject_1 <- scRNA.SeuObj
+  seuratObject_1@meta.data[["DataSetID"]] <- rep("PRJCA001063")
+  rm(list=setdiff(ls(), "seuratObject_1"))
 
-load("D:/Dropbox/##_GitHub/##_CAESAR/MagicDisc/2022-06-23_PDAC_GSE131886_SC/04_Perform_an_integrated_analysis.RData")
-seuratObject_2 <- scRNA.SeuObj
-DefaultAssay(seuratObject_2) <- "RNA"
-rm(list=setdiff(ls(), str_subset(objects(), pattern = "seuratObject")))
+  load("D:/Dropbox/##_GitHub/##_CAESAR/MagicDisc/2022-06-25_PDAC_GSE131886_SC/04_Perform_an_integrated_analysis.RData")
+  seuratObject_2 <- scRNA.SeuObj
+  DefaultAssay(seuratObject_2) <- "RNA"
+  rm(list=setdiff(ls(), str_subset(objects(), pattern = "seuratObject")))
 
 
-load("D:/Dropbox/##_GitHub/##_CAESAR/MagicDisc/2022-06-23_PDAC_GSE154778_SC/04_Perform_an_integrated_analysis.RData")
-seuratObject_3 <- scRNA.SeuObj
-DefaultAssay(seuratObject_3) <- "RNA"
+  load("D:/Dropbox/##_GitHub/##_CAESAR/MagicDisc/2022-06-23_PDAC_GSE154778_SC/04_Perform_an_integrated_analysis.RData")
+  seuratObject_3 <- scRNA.SeuObj
+  DefaultAssay(seuratObject_3) <- "RNA"
 
-scRNA_SeuObj.list <- list(PRJCA001063 = seuratObject_1,
-                          GSE131886 = seuratObject_2,
-                          GSE154778 = seuratObject_3)
+  scRNA_SeuObj.list <- list(PRJCA001063 = seuratObject_1,
+                            GSE131886 = seuratObject_2,
+                            GSE154778 = seuratObject_3)
 
-rm(list=setdiff(ls(), "scRNA_SeuObj.list"))
+  rm(list=setdiff(ls(), "scRNA_SeuObj.list"))
 
 
 ##### Current path and new folder setting* #####
-ProjectName = "TrajAna_PCA"
-Sampletype = "PDAC"
-#ProjSamp.Path = paste0(Sampletype,"_",ProjectName)
+  ProjectName = "TrajAna_PCA"
+  Sampletype = "PDAC"
+  #ProjSamp.Path = paste0(Sampletype,"_",ProjectName)
 
-Version = paste0(Sys.Date(),"_",ProjectName,"_",Sampletype)
-Save.Path = paste0(getwd(),"/",Version)
-## Create new folder
-if (!dir.exists(Save.Path)){
-  dir.create(Save.Path)
-}
+  Version = paste0(Sys.Date(),"_",ProjectName,"_",Sampletype)
+  Save.Path = paste0(getwd(),"/",Version)
+  ## Create new folder
+  if (!dir.exists(Save.Path)){
+    dir.create(Save.Path)
+  }
 
 
 ##### Load Packages #####
@@ -127,22 +127,26 @@ if (!dir.exists(Save.Path)){
 
 
 ##### CombineSeuObj #####
-scRNA.SeuObj <- CombineSeuObj(scRNA_SeuObj.list)
-rm(scRNA_SeuObj.list)
+  scRNA.SeuObj <- CombineSeuObj(scRNA_SeuObj.list)
+  rm(scRNA_SeuObj.list)
 
-# Run the standard workflow for visualization and clustering
-scRNA.SeuObj <- ScaleData(scRNA.SeuObj, verbose = FALSE)
-scRNA.SeuObj <- RunPCA(scRNA.SeuObj, npcs = 1000, verbose = FALSE)
-# scRNA.SeuObj <- RunUMAP(scRNA.SeuObj, reduction = "pca", dims = 1:160,n.neighbors = 20,min.dist = 0.3)
-scRNA.SeuObj <- FindNeighbors(scRNA.SeuObj, reduction = "pca", dims = 1:1000)
-scRNA.SeuObj <- FindClusters(scRNA.SeuObj, resolution = 0.5)
+  # Run the standard workflow for visualization and clustering
+  scRNA.SeuObj <- ScaleData(scRNA.SeuObj, verbose = FALSE)
+  scRNA.SeuObj <- RunPCA(scRNA.SeuObj, npcs = 1000, verbose = FALSE)
+  # scRNA.SeuObj <- RunUMAP(scRNA.SeuObj, reduction = "pca", dims = 1:160,n.neighbors = 20,min.dist = 0.3)
+  scRNA.SeuObj <- FindNeighbors(scRNA.SeuObj, reduction = "pca", dims = 1:1000)
+  scRNA.SeuObj <- FindClusters(scRNA.SeuObj, resolution = 0.5)
 
-##### Plot #####
-scRNA.SeuObj <- RunUMAP(scRNA.SeuObj, reduction = "pca", dims = 1:160,n.neighbors = 20,min.dist = 0.3)
-FeaturePlot(scRNA.SeuObj, features = c("TOP2A"))
-DimPlot(scRNA.SeuObj, reduction = "umap")
-DimPlot(scRNA.SeuObj, reduction = "umap",label = T)
-DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "Cell_type")
+  #### Save RData #####
+  save.image(paste0(Save.Path,"/scRNA.SeuObj_CDS_PRJCA001063_Combine_Ori.RData"))
+
+
+  ##### Plot #####
+  scRNA.SeuObj <- RunUMAP(scRNA.SeuObj, reduction = "pca", dims = 1:160,n.neighbors = 20,min.dist = 0.3)
+  FeaturePlot(scRNA.SeuObj, features = c("TOP2A"))
+  DimPlot(scRNA.SeuObj, reduction = "umap")
+  DimPlot(scRNA.SeuObj, reduction = "umap",label = T)
+  DimPlot(scRNA.SeuObj, reduction = "umap",group.by = "Cell_type")
 
 ##### Export figures #####
 ## Export TIFF
@@ -211,8 +215,8 @@ Idents(scRNA.SeuObj) <- scRNA.SeuObj@meta.data[["ReCluster"]]
 DimPlot(scRNA.SeuObj, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
 FeaturePlot(scRNA.SeuObj, features = c("TOP2A"))
 
-#### Save RData #####
-save.image(paste0(Save.Path,"/scRNA.SeuObj_CDS_PRJCA001063_Combine.RData"))
+  #### Save RData #####
+  save.image(paste0(Save.Path,"/scRNA.SeuObj_CDS_PRJCA001063_Combine_TryCondition.RData"))
 
 
 
