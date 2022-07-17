443cc9600af111da6348f19d2009cff355402c0d
CharleneMSI <q2330416@gmail.com>
2022-07-17 23:22:05 +0800

    Add selection.method

--- FUN_CombineSeuObj.R
+++ FUN_CombineSeuObj.R
@@ -1,4 +1,4 @@
-CombineSeuObj <- function(scRNA_SeuObj.list, nfeatures = 2000 ,...) {
+CombineSeuObj <- function(scRNA_SeuObj.list,selection.method = "vst", nfeatures = 2000 ,...) {
 
   if(length(scRNA_SeuObj.list)==1){
     scRNA.SeuObj <- scRNA_SeuObj.list[[1]]
@@ -7,12 +7,12 @@ CombineSeuObj <- function(scRNA_SeuObj.list, nfeatures = 2000 ,...) {
     set.seed(1) # Fix the seed
     scRNA_SeuObj.list <- lapply(X = scRNA_SeuObj.list, FUN = function(x) {
       x <- NormalizeData(x)
-      x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = nfeatures)
+      x <- FindVariableFeatures(x, selection.method = selection.method, nfeatures = nfeatures)
     })
 
     # select features that are repeatedly variable across datasets for integration
     set.seed(1) # Fix the seed
-    features <- SelectIntegrationFeatures(object.list = scRNA_SeuObj.list)
+    features <- SelectIntegrationFeatures(object.list = scRNA_SeuObj.list, nfeatures = nfeatures)
 
     ## Perform integration
     set.seed(1) # Fix the seed
