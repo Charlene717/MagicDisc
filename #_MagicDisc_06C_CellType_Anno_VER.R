511379beeb179f4b83f53c6c90fba52f7a23a1a5
Charlene <q2330416@gmail.com>
2022-12-12 09:51:04 +0800

    06C_Cell_type_annotation_VER

--- #_MagicDisc_06C_CellType_Anno_VER.R
+++ #_MagicDisc_06C_CellType_Anno_VER.R
@@ -1,44 +1,37 @@
-##### 06 Cell type annotation*  #####
-##### 06 Cell type annotation: Traditional method (Manual annotation by cell type marker)  #####
-# scRNA.SeuObj.copy <- scRNA.SeuObj
+##### Verification (CellCheck) #####
+#### Install ####
+## Check whether the installation of those packages is required
+Package.set <- c("tidyverse","caret","cvms","DescTools","devtools")
+for (i in 1:length(Package.set)) {
+  if (!requireNamespace(Package.set[i], quietly = TRUE)){
+    install.packages(Package.set[i])
+  }
+}
+## Load Packages
+# library(Seurat)
+lapply(Package.set, library, character.only = TRUE)
+rm(Package.set,i)
 
-## CD4+T: CD4+T Cell; CD8+T: CD8+T Cell; T: T Cell; B: B Cell; Mac: Macrophages;
-## Neu: Neutrophils; NK: NK Cell; Mast: Mast Cell; Ery: Erythrocytes;
-## Thr: Thrombocytes
-scRNA.SeuObj <- RenameIdents(scRNA.SeuObj, `0` = "CD4+T", `1` = "B", `2` = "Mac3",
-                             `3` = "Neu", `4` = "CD8+T", `5` = "CD8+T", `6` = "Mac2", `7` = "CD4+T",
-                             `8` = "NK", `9` = "Neu",`10` = "Mast1", `11` = "T", `12` = "Ery", `13` = "Mac1",
-                             `14` = "B", `15` = "B", `16` = "Mast2", `17` = "Mac0", `18` = "Neu")
+## install CellCheck
+# Install the CellCheck package
+detach("package:CellCheck", unload = TRUE)
+devtools::install_github("Charlene717/CellCheck")
+# Load CellCheck
+library(CellCheck)
 
-Cell_Type_Order.set <- c("T", "CD4+T", "CD8+T", "B" , "Mac0", "Mac1", "Mac2", "Mac3", "Mast1", "Mast2", "NK", "Neu", "Ery")
-
-scRNA.SeuObj$celltype <- Idents(scRNA.SeuObj)
-# Idents(scRNA.SeuObj) <- "celltype"
-
-## Heatmap
-Heatmap_Color.lt <- list(low="#5283ff",mid ="white", high ="#ff5c5c")
-scRNA.SeuObj <- Beautify_Heatmap_Seurat(scRNA.SeuObj, PBMC.markers.df, topN = 7, Path = PathCellType,
-                                        Type = "celltype", HMColor.lt = Heatmap_Color.lt,
-                                        projectName = ProjectName)
-
-## Export All DRPlot(UMAP,tSNE)
-Export_All_DRPlot(scRNA.SeuObj)
-
-## Summary
-markers.to.plot <- c("Cd3d","Cd3e", "Cd4","Cd8a", "Csf1r", "Lyz2","Chil3","Il1b", "S100a9","Nkg7",
-                     "Gzmb", "Cd79a", "Ms4a1","Clu","Hbb-bs","Ppbp")
-## DotPlot
-#(Pending)
-BeautifyDotPlot(scRNA.SeuObj, Path = PathCellType, projectName = ProjectName,
-                Features = markers.to.plot)
-
-## Create cell type markers dataframe
-# DefaultAssay(scRNA.SeuObj_Small) <- "RNA"
-scRNA.SeuObj$celltype <- Idents(scRNA.SeuObj)
-CellType.markers.df <- FindAllMarkers(scRNA.SeuObj, only.pos = TRUE, min.pct = 0.1, logfc.threshold = 0.25)
-
-write.table(CellType.markers.df, file = paste0(PathCellType,"/CC_CelltypeMarker_AllGene.txt"),
-            quote = F,sep = "\t",row.names = F)
-
-# #### Save RData ####
-# save.image(paste0(Save.Path,"/06A_Cell_type_annotation_TM.RData"))
+#### Run CellCheck ####
+## Create check dataframe
+scSorter.df <- data.frame(Actual = scRNA.SeuObj_Small@meta.data[["celltype"]],
+                          Predict = scRNA.SeuObj_Small@meta.data[["scSorterPred"]])
+scSorter_Anno.df <- data.frame(TestID = "Predict",
+                               Tool = "scSorter",
+                               Type = "PDAC",
+                               PARM = "1")
+## For one prediction
+DisMultCM.lt <- list(Actual = "Actual", Predict = "Predict", Type = "Type", Type2 = "PDAC" )
+cm_DisMult.lt <- CellCheck_DisMult(scSorter.df, scSorter_Anno.df, Mode = "One", DisMultCM.lt,
+                                   Save.Path = Save.Path, ProjectName = ProjectName)
+## For multiple prediction
+Sum_DisMult.df <- CellCheck_DisMult(scSorter.df, scSorter_Anno.df,
+                                    Mode = "Multiple",DisMultCM.lt=DisMultCM.lt,
+                                    Save.Path = Save.Path, ProjectName = ProjectName)
--- #_MagicDisc_Main.R
+++ #_MagicDisc_Main.R
@@ -159,46 +159,11 @@
   #### Save RData ####
   save.image(paste0(Save.Path,"/06A_Cell_type_annotation_Auto.RData"))
 
-
-
-
   ##### Verification (CellCheck) #####
-    #### Install ####
-    ## Check whether the installation of those packages is required
-    Package.set <- c("tidyverse","caret","cvms","DescTools","devtools")
-    for (i in 1:length(Package.set)) {
-      if (!requireNamespace(Package.set[i], quietly = TRUE)){
-        install.packages(Package.set[i])
-      }
-    }
-    ## Load Packages
-    # library(Seurat)
-    lapply(Package.set, library, character.only = TRUE)
-    rm(Package.set,i)
+  source("#_MagicDisc_06C_CellType_Anno_VER.R")
 
-    ## install CellCheck
-    # Install the CellCheck package
-    detach("package:CellCheck", unload = TRUE)
-    devtools::install_github("Charlene717/CellCheck")
-    # Load CellCheck
-    library(CellCheck)
-
-    #### Run CellCheck ####
-    ## Create check dataframe
-    scSorter.df <- data.frame(Actual = scRNA.SeuObj_Small@meta.data[["celltype"]],
-                              Predict = scRNA.SeuObj_Small@meta.data[["scSorterPred"]])
-    scSorter_Anno.df <- data.frame(TestID = "Predict",
-                                   Tool = "scSorter",
-                                   Type = "PDAC",
-                                   PARM = "1")
-    ## For one prediction
-    DisMultCM.lt <- list(Actual = "Actual", Predict = "Predict", Type = "Type", Type2 = "PDAC" )
-    cm_DisMult.lt <- CellCheck_DisMult(scSorter.df, scSorter_Anno.df, Mode = "One", DisMultCM.lt,
-                                       Save.Path = Save.Path, ProjectName = ProjectName)
-    ## For multiple prediction
-    Sum_DisMult.df <- CellCheck_DisMult(scSorter.df, scSorter_Anno.df,
-                                        Mode = "Multiple",DisMultCM.lt=DisMultCM.lt,
-                                        Save.Path = Save.Path, ProjectName = ProjectName)
+  # #### Save RData ####
+  # save.image(paste0(Save.Path,"/06C_Cell_type_annotation_VER.RData"))
 
 ##### 07 Count Cell number  #####
   source("FUN_AnnoSummary.R")
