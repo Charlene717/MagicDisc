8fabd20acc274cce39213667e3f412afb4bd86e2
Charlene <q2330416@gmail.com>
2022-12-15 21:19:12 +0800

    Setting Para

--- FUN_MetaSummary.R
+++ FUN_MetaSummary.R
@@ -1,5 +1,5 @@
-MetaSummary = function(scRNA_SeuObj.list,  scRNA.SeuObj,
-                       scRNA_SeuObj_QC.list, scRNA_Ori.SeuObj,
+MetaSummary = function(SeuObj_BFQC.list = scRNA_SeuObj.list,  scRNA_BFQC.SeuObj = scRNA_Ori.SeuObj,
+                       SeuObj_AFTQC.list = scRNA_SeuObj_QC.list, scRNA_AFTQC.SeuObj = scRNA.SeuObj,
                        Path = Save.Path,
                        Folder = "B01_CellCount", projectName = ProjectName){
 
@@ -14,30 +14,30 @@ MetaSummary = function(scRNA_SeuObj.list,  scRNA.SeuObj,
   colnames(Meta.df) <- c("Folder","Cell_Num","Gene_Num")
 
   ## Before QC
-  for (i in 1:length(scRNA_SeuObj.list)) {
-    Meta.df[i,1] <- names(scRNA_SeuObj.list[i])
-    Meta.df[i,2] <- ncol(scRNA_SeuObj.list[[i]]@assays[["RNA"]]@counts)
-    Meta.df[i,3] <- nrow(scRNA_SeuObj.list[[i]]@assays[["RNA"]]@counts)
+  for (i in 1:length(SeuObj_BFQC.list)) {
+    Meta.df[i,1] <- names(SeuObj_BFQC.list[i])
+    Meta.df[i,2] <- ncol(SeuObj_BFQC.list[[i]]@assays[["RNA"]]@counts)
+    Meta.df[i,3] <- nrow(SeuObj_BFQC.list[[i]]@assays[["RNA"]]@counts)
 
   }
 
   # Summary to Meta table
   Meta.df[i+1,1] <- c("Summary")
-  Meta.df[i+1,2] <- ncol(scRNA_Ori.SeuObj@assays[["RNA"]]@counts)
-  Meta.df[i+1,3] <- nrow(scRNA_Ori.SeuObj@assays[["RNA"]]@counts)
+  Meta.df[i+1,2] <- ncol(scRNA_BFQC.SeuObj@assays[["RNA"]]@counts)
+  Meta.df[i+1,3] <- nrow(scRNA_BFQC.SeuObj@assays[["RNA"]]@counts)
 
   ## After QC
-  for (j in 1:length(scRNA_SeuObj_QC.list)) {
-    Meta.df[i+j+1,1] <- paste0(names(scRNA_SeuObj_QC.list[j]),".QC")
-    Meta.df[i+j+1,2] <- ncol(scRNA_SeuObj_QC.list[[j]]@assays[["RNA"]]@counts)
-    Meta.df[i+j+1,3] <- nrow(scRNA_SeuObj_QC.list[[j]]@assays[["RNA"]]@counts)
+  for (j in 1:length(SeuObj_AFTQC.list)) {
+    Meta.df[i+j+1,1] <- paste0(names(SeuObj_AFTQC.list[j]),".QC")
+    Meta.df[i+j+1,2] <- ncol(SeuObj_AFTQC.list[[j]]@assays[["RNA"]]@counts)
+    Meta.df[i+j+1,3] <- nrow(SeuObj_AFTQC.list[[j]]@assays[["RNA"]]@counts)
 
   }
 
   # Summary to Meta table
   Meta.df[i+j+2,1] <- c("Summary.QC")
-  Meta.df[i+j+2,2] <- ncol(scRNA.SeuObj@assays[["RNA"]]@counts)
-  Meta.df[i+j+2,3] <- nrow(scRNA.SeuObj@assays[["RNA"]]@counts)
+  Meta.df[i+j+2,2] <- ncol(scRNA_AFTQC.SeuObj@assays[["RNA"]]@counts)
+  Meta.df[i+j+2,3] <- nrow(scRNA_AFTQC.SeuObj@assays[["RNA"]]@counts)
 
 
   rm(i,j)
